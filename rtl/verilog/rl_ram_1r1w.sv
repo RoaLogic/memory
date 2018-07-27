f2613d4ca92d8676ebd06c30cdc0d1790e509b1e
Richard Herveille <richard.herveille@roalogic.com>
2018-07-27 10:07:55 +0200

    Updated logo and port names

--- rtl/verilog/rl_ram_1r1w.sv
+++ rtl/verilog/rl_ram_1r1w.sv
@@ -1,46 +1,69 @@
-/////////////////////////////////////////////////////////////////
-//                                                             //
-//    ██████╗  ██████╗  █████╗                                 //
-//    ██╔══██╗██╔═══██╗██╔══██╗                                //
-//    ██████╔╝██║   ██║███████║                                //
-//    ██╔══██╗██║   ██║██╔══██║                                //
-//    ██║  ██║╚██████╔╝██║  ██║                                //
-//    ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝                                //
-//          ██╗      ██████╗  ██████╗ ██╗ ██████╗              //
-//          ██║     ██╔═══██╗██╔════╝ ██║██╔════╝              //
-//          ██║     ██║   ██║██║  ███╗██║██║                   //
-//          ██║     ██║   ██║██║   ██║██║██║                   //
-//          ███████╗╚██████╔╝╚██████╔╝██║╚██████╗              //
-//          ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝ ╚═════╝              //
-//                                                             //
-//    1R1W RAM Block                                           //
-//                                                             //
-/////////////////////////////////////////////////////////////////
-//                                                             //
-//    Copyright (C) 2015-2016 ROA Logic BV                     //
-//    www.roalogic.com                                         //
-//                                                             //
-//   This source file may be used and distributed without      //
-// restriction provided that this copyright statement is not   //
-// removed from the file and that any derivative work contains //
-// the original copyright notice and the associated disclaimer.//
-//                                                             //
-//     THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY     //
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   //
-// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS   //
-// FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR      //
-// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,         //
-// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    //
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE   //
-// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR        //
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  //
-// LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR TORT  //
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  //
-// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         //
-// POSSIBILITY OF SUCH DAMAGE.                                 //
-//                                                             //
-/////////////////////////////////////////////////////////////////
- 
+/////////////////////////////////////////////////////////////////////
+//   ,------.                    ,--.                ,--.          //
+//   |  .--. ' ,---.  ,--,--.    |  |    ,---. ,---. `--' ,---.    //
+//   |  '--'.'| .-. |' ,-.  |    |  |   | .-. | .-. |,--.| .--'    //
+//   |  |\  \ ' '-' '\ '-'  |    |  '--.' '-' ' '-' ||  |\ `--.    //
+//   `--' '--' `---'  `--`--'    `-----' `---' `-   /`--' `---'    //
+//                                             `---'               //
+//   1R1W RAM Block                                                //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+//                                                                 //
+//             Copyright (C) 2015-2018 Roa Logic BV                //
+//             www.roalogic.com                                    //
+//                                                                 //
+//   This source file may be used and distributed without          //
+//   restriction provided that this copyright statement is not     //
+//   removed from the file and that any derivative work contains   //
+//   the original copyright notice and the associated disclaimer.  //
+//                                                                 //
+//      THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY        //
+//   EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED     //
+//   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     //
+//   FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR OR     //
+//   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  //
+//   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  //
+//   NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  //
+//   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)      //
+//   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     //
+//   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR  //
+//   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS          //
+//   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+
+// +FHDR -  Semiconductor Reuse Standard File Header Section  -------
+// FILE NAME      : rl_ram_1r1w.sv
+// DEPARTMENT     :
+// AUTHOR         : rherveille
+// AUTHOR'S EMAIL :
+// ------------------------------------------------------------------
+// RELEASE HISTORY
+// VERSION DATE        AUTHOR      DESCRIPTION
+// 1.0     2018-07-27  rherveille  initial release with new logo
+// ------------------------------------------------------------------
+// KEYWORDS : MEMORY RAM 1R1W
+// ------------------------------------------------------------------
+// PURPOSE  : Wrapper for technology specific 1R1W RAM Blocks
+// ------------------------------------------------------------------
+// PARAMETERS
+//  PARAM NAME        RANGE  DESCRIPTION              DEFAULT UNITS
+//  ABITS             1+     Number of address bits   10      bits
+//  DBITS             1+     Number of data bits      32      bits
+//  TECHNOLOGY               Technology Name          GENERIC
+// ------------------------------------------------------------------
+// REUSE ISSUES 
+//   Reset Strategy      : asynchronous, active low
+//   Clock Domains       : clk                      
+//   Critical Timing     : 
+//   Test Features       : 
+//   Asynchronous I/F    : none                     
+//   Scan Methodology    : na
+//   Instantiations      : Yes; technology specific macros
+//   Synthesizable (y/n) : Yes
+//   Other               : 
+// -FHDR-------------------------------------------------------------
+
 
 module rl_ram_1r1w #(
   parameter ABITS      = 10,
@@ -48,19 +71,19 @@ module rl_ram_1r1w #(
   parameter TECHNOLOGY = "GENERIC"
 )
 (
-  input                    rstn,
-  input                    clk,
+  input                    rst_ni,
+  input                    clk_i,
  
   //Write side
-  input  [ ABITS     -1:0] waddr,
-  input  [ DBITS     -1:0] din,
-  input                    we,
-  input  [(DBITS+7)/8-1:0] be,
+  input  [ ABITS     -1:0] waddr_i,
+  input  [ DBITS     -1:0] din_i,
+  input                    we_i,
+  input  [(DBITS+7)/8-1:0] be_i,
 
   //Read side
-  input  [ ABITS     -1:0] raddr,
-  input                    re,
-  output [ DBITS     -1:0] dout
+  input  [ ABITS     -1:0] raddr_i,
+  input                    re_i,
+  output [ DBITS     -1:0] dout_o
 );
   //////////////////////////////////////////////////////////////////
   //
@@ -87,9 +110,17 @@ generate
         .ABITS ( ABITS ),
         .DBITS ( DBITS ) )
       ram_inst (
-        .dout ( mem_dout   ),
-        .re   (~contention ),
-        .*
+        .rst_ni  ( rst_ni     ),
+        .clk_i   ( clk_i      ),
+
+        .waddr_i ( waddr_i    ),
+        .din_i   ( din_i      ),
+        .we_i    ( we_i       ),
+        .be_i    ( be_i       ),
+
+        .raddr_i ( raddr_i    ),
+        .re_i    (~contention ),
+        .dout_o  ( mem_dout   )
       );
   end
   else
@@ -102,10 +133,18 @@ generate
       rl_ram_1r1w_easic_n3x #(
         .ABITS ( ABITS ),
         .DBITS ( DBITS ) )
-      ram_inst ( 
-        .dout ( mem_dout   ),
-        .re   (~contention ),
-        .*
+      ram_inst (
+        .rst_ni  ( rst_ni     ),
+        .clk_i   ( clk_i      ),
+
+        .waddr_i ( waddr_i    ),
+        .din_i   ( din_i      ),
+        .we_i    ( we_i       ),
+        .be_i    ( be_i       ),
+
+        .raddr_i ( raddr_i    ),
+        .re_i    (~contention ),
+        .dout_o  ( mem_dout   )
       );
   end
   else // (TECHNOLOGY == "GENERIC")
@@ -119,8 +158,17 @@ generate
         .ABITS ( ABITS ),
         .DBITS ( DBITS ) )
       ram_inst (
-        .dout ( mem_dout ),
-        .*
+        .rst_ni  ( rst_ni   ),
+        .clk_i   ( clk_i    ),
+
+        .waddr_i ( waddr_i  ),
+        .din_i   ( din_i    ),
+        .we_i    ( we_i     ),
+        .be_i    ( be_i     ),
+
+        .raddr_i ( raddr_i  ),
+        .re_i    ( re_i     ),
+        .dout_o  ( mem_dout )
       );
   end
 endgenerate
@@ -129,15 +177,15 @@ endgenerate
   //TODO Handle 'be' ... requires partial old, partial new data
 
   //now ... write-first; we'll still need some bypass logic
-  assign contention = we && (raddr == waddr) ? re : 1'b0; //prevent 'x' from propagating from eASIC memories
+  assign contention = we_i && (raddr_i == waddr_i) ? re_i : 1'b0; //prevent 'x' from propagating from eASIC memories
 
-  always @(posedge clk)
+  always @(posedge clk_i)
   begin
       contention_reg <= contention;
       din_dly        <= din;
   end
 
-  assign dout = contention_reg ? din_dly : mem_dout;
+  assign dout_o = contention_reg ? din_dly : mem_dout;
 endmodule
 
 
--- rtl/verilog/rl_ram_1rw.sv
+++ rtl/verilog/rl_ram_1rw.sv
@@ -1,46 +1,70 @@
-/////////////////////////////////////////////////////////////////
-//                                                             //
-//    ██████╗  ██████╗  █████╗                                 //
-//    ██╔══██╗██╔═══██╗██╔══██╗                                //
-//    ██████╔╝██║   ██║███████║                                //
-//    ██╔══██╗██║   ██║██╔══██║                                //
-//    ██║  ██║╚██████╔╝██║  ██║                                //
-//    ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝                                //
-//          ██╗      ██████╗  ██████╗ ██╗ ██████╗              //
-//          ██║     ██╔═══██╗██╔════╝ ██║██╔════╝              //
-//          ██║     ██║   ██║██║  ███╗██║██║                   //
-//          ██║     ██║   ██║██║   ██║██║██║                   //
-//          ███████╗╚██████╔╝╚██████╔╝██║╚██████╗              //
-//          ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝ ╚═════╝              //
-//                                                             //
-//    1RW (SP) RAM Block                                       //
-//                                                             //
-/////////////////////////////////////////////////////////////////
-//                                                             //
-//    Copyright (C) 2014-2016 ROA Logic BV                     //
-//    www.roalogic.com                                         //
-//                                                             //
-//   This source file may be used and distributed without      //
-// restriction provided that this copyright statement is not   //
-// removed from the file and that any derivative work contains //
-// the original copyright notice and the associated disclaimer.//
-//                                                             //
-//     THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY     //
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   //
-// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS   //
-// FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR      //
-// OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,         //
-// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES    //
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE   //
-// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR        //
-// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF  //
-// LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR TORT  //
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT  //
-// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         //
-// POSSIBILITY OF SUCH DAMAGE.                                 //
-//                                                             //
-/////////////////////////////////////////////////////////////////
- 
+/////////////////////////////////////////////////////////////////////
+//   ,------.                    ,--.                ,--.          //
+//   |  .--. ' ,---.  ,--,--.    |  |    ,---. ,---. `--' ,---.    //
+//   |  '--'.'| .-. |' ,-.  |    |  |   | .-. | .-. |,--.| .--'    //
+//   |  |\  \ ' '-' '\ '-'  |    |  '--.' '-' ' '-' ||  |\ `--.    //
+//   `--' '--' `---'  `--`--'    `-----' `---' `-   /`--' `---'    //
+//                                             `---'               //
+//   1RW (SP) Memory Block                                         //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+//                                                                 //
+//             Copyright (C) 2014-2018 Roa Logic BV                //
+//             www.roalogic.com                                    //
+//                                                                 //
+//   This source file may be used and distributed without          //
+//   restriction provided that this copyright statement is not     //
+//   removed from the file and that any derivative work contains   //
+//   the original copyright notice and the associated disclaimer.  //
+//                                                                 //
+//      THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY        //
+//   EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED     //
+//   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS     //
+//   FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR OR     //
+//   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,  //
+//   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT  //
+//   NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;  //
+//   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)      //
+//   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     //
+//   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR  //
+//   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS          //
+//   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+
+// +FHDR -  Semiconductor Reuse Standard File Header Section  -------
+// FILE NAME      : rl_ram_1rw.sv
+// DEPARTMENT     :
+// AUTHOR         : rherveille
+// AUTHOR'S EMAIL :
+// ------------------------------------------------------------------
+// RELEASE HISTORY
+// VERSION DATE        AUTHOR      DESCRIPTION
+// 1.0     2018-07-27  rherveille  initial release with new logo
+// ------------------------------------------------------------------
+// KEYWORDS : MEMORY RAM 1RW Single Port
+// ------------------------------------------------------------------
+// PURPOSE  : Wrapper for technology specific 1RW RAM Blocks
+// ------------------------------------------------------------------
+// PARAMETERS
+//  PARAM NAME        RANGE  DESCRIPTION              DEFAULT UNITS
+//  ABITS             1+     Number of address bits   10      bits
+//  DBITS             1+     Number of data bits      32      bits
+//  TECHNOLOGY               Technology Name          GENERIC
+// ------------------------------------------------------------------
+// REUSE ISSUES 
+//   Reset Strategy      : rstn_i; asynchronous, active low
+//   Clock Domains       : clk_i; rising edge
+//   Critical Timing     : 
+//   Test Features       : 
+//   Asynchronous I/F    : none                     
+//   Scan Methodology    : na
+//   Instantiations      : Yes; eip_n3xs_rfile_array
+//                              eip_n3xs_bram_array
+//   Synthesizable (y/n) : Yes
+//   Other               : 
+// -FHDR-------------------------------------------------------------
+
 
 module rl_ram_1rw #(
   parameter ABITS      = 10,
@@ -48,14 +72,14 @@ module rl_ram_1rw #(
   parameter TECHNOLOGY = "GENERIC"
 )
 (
-  input                    rstn,
-  input                    clk,
+  input                    rst_ni,
+  input                    clk_i,
 
-  input  [ ABITS     -1:0] addr,
-  input                    we,
-  input  [(DBITS+7)/8-1:0] be,
-  input  [ DBITS     -1:0] din,
-  output [ DBITS     -1:0] dout
+  input  [ ABITS     -1:0] addr_i,
+  input                    we_i,
+  input  [(DBITS+7)/8-1:0] be_i,
+  input  [ DBITS     -1:0] din_i,
+  output [ DBITS     -1:0] dout_o
 );
   //////////////////////////////////////////////////////////////////
   //
@@ -72,7 +96,16 @@ generate
       rl_ram_1rw_easic_n3x #(
         .ABITS ( ABITS ),
         .DBITS ( DBITS ) )
-      ram_inst ( .* );
+      ram_inst (
+        .rst_ni ( rst_ni ),
+        .clk_i  ( clk_i  ),
+
+        .addr_i ( addr_i ),
+        .we_i   ( we_i   ),
+        .be_i   ( be_i   ),
+        .din_i  ( din_i  ),
+        .dout_o ( dout_o )
+      );
   end
   else // (TECHNOLOGY == "GENERIC")
   begin
@@ -84,7 +117,16 @@ generate
       rl_ram_1rw_generic #(
         .ABITS ( ABITS ),
         .DBITS ( DBITS ) )
-      ram_inst ( .* );
+      ram_inst (
+        .rst_ni ( rst_ni ),
+        .clk_i  ( clk_i  ),
+
+        .addr_i ( addr_i ),
+        .we_i   ( we_i   ),
+        .be_i   ( be_i   ),
+        .din_i  ( din_i  ),
+        .dout_o ( dout_o )
+      );
   end
 
 endgenerate
