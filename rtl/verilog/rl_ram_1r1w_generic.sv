8d233330f0dda29c231ccbc7704e83af789a97e9
Richard Herveille <richard.herveille@roalogic.com>
2023-12-20 03:37:59 +0100

    Mix read/write port width support

--- rtl/verilog/rl_ram_1r1w_generic.sv
+++ rtl/verilog/rl_ram_1r1w_generic.sv
@@ -65,24 +65,60 @@
 
 
 module rl_ram_1r1w_generic #(
-  parameter ABITS      = 10,
-  parameter DBITS      = 32,
-  parameter INIT_FILE  = ""
+  parameter ABITS       = 10,
+  parameter DBITS       = 32,
+  parameter WRITE_ABITS = ABITS,
+  parameter WRITE_DBITS = DBITS,
+  parameter READ_ABITS  = ABITS,
+  parameter READ_DBITS  = DBITS,
+  parameter INIT_FILE   = ""
 )
 (
-  input                        rst_ni,
-  input                        clk_i,
+  input                              rst_ni,
+  input                              clk_i,
 
   //Write side
-  input      [ ABITS     -1:0] waddr_i,
-  input      [ DBITS     -1:0] din_i,
-  input                        we_i,
-  input      [(DBITS+7)/8-1:0] be_i,
+  input      [ WRITE_ABITS     -1:0] waddr_i,
+  input      [ WRITE_DBITS     -1:0] din_i,
+  input                              we_i,
+  input      [(WRITE_DBITS+7)/8-1:0] be_i,
 
   //Read side
-  input      [ ABITS     -1:0] raddr_i,
-  output reg [ DBITS     -1:0] dout_o
+  input      [ READ_ABITS      -1:0] raddr_i,
+  output reg [ READ_DBITS      -1:0] dout_o
 );
+
+  //////////////////////////////////////////////////////////////////
+  //
+  // Functions
+  //
+  function is_power_of_2(input int n);
+    is_power_of_2 = (n & (n-1)) == 0;
+  endfunction
+
+  function int max(input int a, input int b);
+    max = a > b ? a : b;
+  endfunction : max
+
+
+  //////////////////////////////////////////////////////////////////
+  //
+  // Constants
+  //
+
+  localparam int RDWR_RATIO = max(READ_DBITS  / WRITE_DBITS, 1);
+  localparam int WRRD_RATIO = max(WRITE_DBITS / READ_DBITS,  1);
+
+  localparam int SEL_RD     = RDWR_RATIO > WRRD_RATIO;
+
+  localparam int RATIO      = SEL_RD ? RDWR_RATIO    : WRRD_RATIO;
+  localparam int RAM_DBITS  = SEL_RD ? WRITE_DBITS   : READ_DBITS;
+  localparam int RAM_DEPTH  = SEL_RD ? 2**READ_ABITS : 2**WRITE_ABITS;
+  localparam int RAM_BANKS  = SEL_RD ? 2**(WRITE_ABITS - READ_ABITS) : 2**(READ_ABITS - WRITE_ABITS);
+
+  localparam int RD_TOTAL_BITS = 2**READ_ABITS  * READ_DBITS;
+  localparam int WR_TOTAL_BITS = 2**WRITE_ABITS * WRITE_DBITS;
+
 
   //////////////////////////////////////////////////////////////////
   //
@@ -90,7 +126,7 @@ module rl_ram_1r1w_generic #(
   //
   genvar i;
 
-  logic [DBITS-1:0] mem_array [2**ABITS -1:0];  //memory array
+  logic [RAM_BANKS-1:0][RAM_DBITS-1:0] mem_array [RAM_DEPTH];  //memory array
 
 
   //////////////////////////////////////////////////////////////////
@@ -98,41 +134,87 @@ module rl_ram_1r1w_generic #(
   // Module Body
   //
 
-  //preload memory
-  //This seems to be synthesizable in FPGAs
   initial
-    if (INIT_FILE != "")
-    begin
-        $display ("INFO   : Loading %s (%m)", INIT_FILE);
-        $readmemh(INIT_FILE, mem_array);
-    end
+  begin
+      //preload memory
+      //This seems to be synthesizable in FPGAs
+      if (INIT_FILE != "")
+      begin
+          $display ("INFO   : Loading %s (%m)", INIT_FILE);
+          $readmemh(INIT_FILE, mem_array);
+      end
 
+      //Checks
+      if (RD_TOTAL_BITS != WR_TOTAL_BITS)
+      begin
+          $error("Total number of read port bits (%0d) is not equal to the total number of write port bits(%0d) (%m)", RD_TOTAL_BITS, WR_TOTAL_BITS);
+          $finish;
+      end
+
+      if ( !is_power_of_2(RATIO) )
+      begin
+          $error("Ratio between read and write ports must be a power of 2");
+          $finish;
+      end
+  end
 
   /*
    * write side
    */
 generate
-  for (i=0; i<(DBITS+7)/8; i++)
-  begin: write
-     if (i*8 +8 > DBITS)
-     begin
-         always @(posedge clk_i)
-           if (we_i && be_i[i]) mem_array[ waddr_i ] [DBITS-1:i*8] <= din_i[DBITS-1:i*8];
-     end
-     else
-     begin
-         always @(posedge clk_i)
-           if (we_i && be_i[i]) mem_array[ waddr_i ][i*8+:8] <= din_i[i*8+:8];
-     end
+  if (SEL_RD)
+  begin
+      /* Write
+       */
+      for (i=0; i<(WRITE_DBITS+7)/8; i++)
+      begin: write
+         if (i*8 +8 > WRITE_DBITS)
+         begin
+             always @(posedge clk_i)
+               if (we_i && be_i[i])
+                 mem_array[waddr_i / RDWR_RATIO][waddr_i % RDWR_RATIO][WRITE_DBITS-1:i*8] <= din_i[WRITE_DBITS-1:i*8];
+         end
+         else
+         begin
+             always @(posedge clk_i)
+               if (we_i && be_i[i])
+                 mem_array[waddr_i /RDWR_RATIO][waddr_i % RDWR_RATIO][i*8+:8] <= din_i[i*8+:8];
+         end
+      end
+
+      /* Read
+       */
+      //per Altera's recommendations. Prevents bypass logic
+      always @(posedge clk_i)
+        dout_o <= mem_array[raddr_i / WRRD_RATIO];
+  end
+  else
+  begin
+      /* Write
+       */
+      for (i=0; i<(WRITE_DBITS+7)/8; i++)
+      begin: write
+         if (i*8 +8 > WRITE_DBITS)
+         begin
+             always @(posedge clk_i)
+               if (we_i && be_i[i])
+                 mem_array[waddr_i / RDWR_RATIO][waddr_i % RDWR_RATIO][WRITE_DBITS-1:i*8] <= din_i[WRITE_DBITS-1:i*8];
+         end
+         else
+         begin
+             always @(posedge clk_i)
+               if (we_i && be_i[i])
+                 mem_array[waddr_i /RDWR_RATIO][waddr_i % RDWR_RATIO][i*8+:8] <= din_i[i*8+:8];
+         end
+      end
+
+      /* Read
+       */
+      //per Altera's recommendations. Prevents bypass logic
+      always @(posedge clk_i)
+        dout_o <= mem_array[raddr_i / WRRD_RATIO][raddr_i % WRRD_RATIO];
   end
 endgenerate
-
-  /*
-   * read side
-   */
-  //per Altera's recommendations. Prevents bypass logic
-  always @(posedge clk_i)
-    dout_o <= mem_array[ raddr_i ];
 endmodule
 
 
